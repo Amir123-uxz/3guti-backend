75c5fa255b1fb4da6fa75e60de11838742317a3c
Amir123-uxz <quotexhlp@gmail.com>
2025-07-08 06:52:41 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,1 +1,10 @@
-// user schema
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  mobile: { type: String, unique: true, required: true },
+  passwordHash: { type: String, required: true },
+  wallet: { type: Number, default: 0 }, // in points/₹
+});
+
+module.exports = mongoose.model('User', userSchema);
