afcf5587caa15702a8601f871b41f70992dab06b
Amir123-uxz <quotexhlp@gmail.com>
2025-07-08 06:54:28 +0530

    Update Match.js

--- Match.js
+++ Match.js
@@ -1,1 +1,14 @@
-// match schema
+// models/Match.js
+const mongoose = require('mongoose');
+
+const matchSchema = new mongoose.Schema({
+  room: { type: String, required: true },
+  players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  betAmount: { type: Number, required: true },
+  winner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  commission: { type: Number }, // 5% cut
+  status: { type: String, enum: ['ongoing','completed'], default: 'ongoing' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Match', matchSchema);
